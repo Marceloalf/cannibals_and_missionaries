a8cd6cecf0f028e9e84c3c7cb42b2884e53483a0
Marcelo Feitosa <marcelo.augusto.lima.feitosa@gmail.com>
2021-12-14 05:54:18 -0300

    "feat: adding a function to calculate possible status from previous status"

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ POSSIBLE_MOVES = [
     (1, 0),
     (2, 0),
 ]
+
+
+def possibilities(status):
+    for movements in POSSIBLE_MOVES:
+        s = move(status, *movements)
+
+
+initial_status = Status()
+possibilities(initial_status)
+
