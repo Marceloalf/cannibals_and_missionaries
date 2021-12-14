067c89a8b63c87e1800b1c664c2e60ca424cece2
Marcelo Feitosa <marcelo.augusto.lima.feitosa@gmail.com>
2021-12-14 05:45:50 -0300

    "feat: adding possibles moves to the boat"

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from status_class import *
 
-s1 = Status()
-a = move(s1, 2, 0)
-b = move(a, 1, 0)
 
-print("Oi")
+POSSIBLE_MOVES = [
+    (0, 1),
+    (0, 2),
+    (1, 1),
+    (1, 0),
+    (2, 0),
+]
